47be4add910e2a4004240f4c7066e9a11dfde3d2
dmytro26pyvovar <133111698+dmytro26pyvovar@users.noreply.github.com>
2023-09-13 12:36:21 +0200

    Create 01-gallery.js

---
+++ js/01-gallery.js
@@ -0,0 +1,32 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(link => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${link.largeImage}">
+        <img
+          class="gallery__image"
+          src="${link.smallImage}"
+          data-source="${link.largeImage}"
+          alt="${link.imageDescription}"
+          loading="lazy"
+        />
+      </a>
+    </li>
+  `)
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+gallery.addEventListener('click', handleClick);
+  
+function handleClick(event) {
+  event.preventDefault();
+    const source = event.target.dataset.source;
+    const description = event.target.alt;
+  const instance = basicLightBox.create(`
+  <div class = "box"><img class ="gallery__image" src="${source}" alt="${description}"></div>`);
+  instance.show();
+}
+
