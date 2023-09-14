89b71bc5074d82688d82d07983983541b66c1801
dmytro26pyvovar <133111698+dmytro26pyvovar@users.noreply.github.com>
2023-09-14 10:41:11 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,16 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const gallery = document.querySelector('.gallery');
-const markup = galleryItems.map(link => `
-    <li class="gallery__item">
-   <a class="gallery__link" href="large-image.jpg">
-            <img
-                class="gallery__image"
-                src="small-image.jpg"
-                alt="Image description" />
-   </a>
-</li>
- `).join('');
- gallery.insertAdjacentHTML('beforeend', markup)
 
-console.log(galleryItems);
+const markup = 
+     galleryItems.map(image => `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${image.original}">
+                <img class="gallery__image" src="${image.preview}" alt="${image.description}" loading="lazy" />
+            </a>
+        </li>
+    `).join('');
+
+gallery.insertAdjacentHTML('beforeend', markup)
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt', 
+    captionDelay: 250
+});
